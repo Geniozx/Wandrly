5633cff9f6c848a8295e8a0e563ae78f477d68d6
GenioZx <eli.rodri14@email.com>
2025-09-14 15:44:24 -0400

    defined post index route

--- wandrly_app/urls.py
+++ wandrly_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('posts/', views.post_index, name='post-index'),
 ]
 
--- wandrly_app/views.py
+++ wandrly_app/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+
+# views.py
+
+class Post:
+    def __init__(self, media, caption, location, likes, createdAt, comments):
+        self.media = media
+        self.caption = caption
+        self.location = location
+        self.likes = likes
+        self.createdAt = createdAt
+        self.comments = comments
+
+# Create a list of Cat instances
+posts = [
+    Post('image', 'I Went Somewhere', 'New York', 'Likes', '2 seconds ago', 'that is awesome'),
+]
+
+
+
 # Create your views here.
 def home(request):
     return HttpResponse('<h1>This is my post</h1>')
+
+def post_index(request):
+    return render(request, 'posts/index.html', {'posts': posts})
