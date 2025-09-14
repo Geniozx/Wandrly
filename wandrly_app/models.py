d84e7185e20e56b826a6f1ca573cac5368820229
GenioZx <eli.rodri14@email.com>
2025-09-14 19:47:51 -0400

    created more models, created superuser, migrated, installed pillow for uploading files

--- wandrly_app/models.py
+++ wandrly_app/models.py
@@ -1,13 +1,42 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your models here.
+# class User(AbstractUser):
+#     bio = models.TextField(max_length=250, blank=True)
+#     profile_image = models.ImageField(uplaod_to="profiles/", blank=True, null=True)
+
+#     def __str__(self):
+#         return self.username
+
+
 class Post(models.Model):
-    user = models.ForeignKey(User, on_delte=CASCADE, related_name="posts")
+    # user = models.ForeignKey(User, on_delte=CASCADE, related_name="posts")
     media = models.ImageField(upload_to="posts/")
-    caption = models.TextField(max_length=2200, blank=True)
-    location = models.ForeignKey(
-        Location, on_delete=models.SET_NULL, null=True, related_names="posts"
-    )
-    likes = models.ManyToManyField(User, related_name="liked_posts", blank=True)
+    caption = models.TextField(blank=True)
+    # location = models.ForeignKey(
+    #     Location, on_delete=models.SET_NULL, null=True, related_names="posts"
+    # )
+    # likes = models.ManyToManyField(User, related_name="liked_posts", blank=True)
     createdAt = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f"{self.user.username}: {self.caption[:2200]}"
+    
+class Location(models.Model):
+    continent = models.CharField(max_length=50)
+    continent_code = models.CharField(max_length=5)
+
+    country = models.CharField(max_length=100)
+    state = models.CharField(max_length=100, blank=True, null=True)
+    city = models.CharField(max_length=100, blank=True, null=True)
+    district = models.CharField(max_length=100, blank=True, null=True)
+    zipcode = models.CharField(max_length=100, blank=True, null=True)
+
+    latitude = models.FloatField(blank=True, null=True)
+    longitude = models.FloatField(blank=True, null=True)
+
+    def __str__(self):
+        parts = [self.city, self.state, self.coutry, self.continent]
+        return ", ".join([p for p in parts if p])
--- wandrly_app/views.py
+++ wandrly_app/views.py
@@ -4,25 +4,10 @@ from django.shortcuts import render
 
 # views.py
 
-class Post:
-    def __init__(self, user, media, caption, location, likes, createdAt):
-        self.user = user
-        self.media = media
-        self.caption = caption
-        self.location = location
-        self.likes = likes
-        self.createdAt = createdAt
-
-# Create a list of Cat instances
-posts = [
-    Post('Test','image', 'I Went Somewhere', 'New York', 'Likes', '2 seconds ago'),
-]
-
-
-
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def post_index(request):
+    posts = Post.objects.all()
     return render(request, 'posts/index.html', {'posts': posts})
