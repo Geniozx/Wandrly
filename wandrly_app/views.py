2d8823357e1d1bcb067113dff7c0085cb9bb27ee
GenioZx <eli.rodri14@email.com>
2025-09-17 13:25:58 -0400

    2d map showing on index and detail html page.

--- wandrly_app/views.py
+++ wandrly_app/views.py
@@ -9,12 +9,37 @@ def home(request):
 def post_index(request):
     posts = Post.objects.all().order_by('-created_at')
     comment_form = CommentForm
-    return render(request, 'posts/index.html', {'posts': posts, 'comment_form': comment_form})
+    coords = [
+        {
+            "lat": p.location.coordinates.y,
+            "lng": p.location.coordinates.x,
+            "caption": p.caption,
+            "city": p.location.city,
+            "country": p.location.country,
+            "id": p.id
+        }
+        for p in posts if p.location and p.location.coordinates
+    ]
+    return render(request, 'posts/index.html', {
+        'posts': posts,
+        'comment_form': comment_form,
+        'coords': coords
+    })
 
 def post_detail(request, post_id):
     post = get_object_or_404(Post, id=post_id)
     comment_form = CommentForm
-    return render(request, 'posts/detail.html', {'post': post, 'comment_form': comment_form})
+    coords = None
+    if post.location and post.location.coordinates:
+        coords = {
+            "lat": post.location.coordinates.y,
+            "lng": post.location.coordinates.x,
+        }
+    return render(request, 'posts/detail.html', {
+        'post': post,
+        'comment_form': comment_form,
+        'coords': coords
+    })
 
 def add_comment(request, post_id):
     form = CommentForm(request.POST)
@@ -35,5 +60,8 @@ class PostUpdate(UpdateView):
 class PostDelete(DeleteView):
     model = Post
     success_url = '/posts/'
+
+
+
 
 
